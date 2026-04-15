07c678c39e40503a9e7896c6c73450b4d29b2d89
Ujjval Yadav <ujjvalyadav746@gmail.com>
2026-04-15 18:37:06 +0530

    utils code, apirasponse, errorHandler

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,17 @@
 import "dotenv/config";
 import connectDB from "./db/db.js";
+import express from "express"
+
+const app = express()
 
 console.log(process.env.PORT);
 
 connectDB()
-
-
-/*
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("errror", (error) => {
-            console.log("ERR: ", error);
-            throw error
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })
-    } catch (error) {
-        console.error("ERROR: ", error)
-        throw err
-    }
-}) ()
-*/
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);    
+    })
+})
+.catch((err)=> {
+    console.log("mongoDB connection failed !! ", err);   
+})
